d82894d75486379313063d94d6535b74a8e24322
Plain Jane <jane99hsu@gmail.com>
2020-12-11 15:36:26 -0800

    added server.js code to use express, mongoose, and mongo

---
+++ server.js
@@ -0,0 +1,27 @@
+// set up mongoose to connect when we start the app
+const mongoose = require('mongoose');
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// tell mongoose which database we want to connect to
+// if MONGODB_URI exists, then use that
+// otherwise use the localhost
+// mongodb will find and connect to the database if it exists or create the database if it doesn't
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
