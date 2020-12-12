1f0cf6801eaad7b5086f981ac1bb25400a0e8227
Plain Jane <jane99hsu@gmail.com>
2020-12-11 16:37:58 -0800

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,38 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const dateFormat = require('../utils/dateFormat');
+
+// just schema required since reaction data will never be queried directly
+const ReactionSchema = new Schema(
+    {
+        // unique identifier needed to avoid confusion with parent _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 // define the thought schema 
 const ThoughtSchema = new Schema(
